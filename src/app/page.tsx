a749ed7da0ea45890ece302fae3670980401027b
Isaac Antwi <antwikisaac@gmail.com>
2025-06-06 05:50:29 +0000

    Added the hero section and its content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/sections/Hero";
 import Navbar from "@/components/shared/Navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-     
+      <Hero />
     </div>
   );
 }
