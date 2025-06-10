2983d28d6de0d15d6e2cb870d2e89dbb4cdeb481
Isaac Antwi <antwikisaac@gmail.com>
2025-06-10 20:10:52 +0000

    Modified the page.tsx

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -3,8 +3,9 @@ import { createClient } from 'next-sanity';
 import imageUrlBuilder from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+const projectId = process.env.SANITY_PROJECT_ID;
+const dataset = process.env.SANITY_DATASET || 'production';
+
 
 if (!projectId) {
   throw new Error('Missing NEXT_PUBLIC_SANITY_PROJECT_ID in environment variables');
